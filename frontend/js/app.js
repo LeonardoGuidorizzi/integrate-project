0bec5cf395f038a2e6c51c7080575a8c50f4e89b
unknown <leonardo.tesoledy@gmail.com>
2022-09-28 01:51:20 -0300

    finalizando

---
+++ frontend/js/app.js
@@ -0,0 +1,46 @@
+// js to index.html
+const containerCards = document.querySelector('.curse-container')
+
+const fetchCursos = async () => {
+    const rawData = await fetch('http://localhost:3000/cursos/');
+    const response = await rawData.json();
+    return response;
+}
+
+const cursos = await fetchCursos();
+
+const createCards = (cursos = []) => {
+    cursos.forEach((curso) => {
+        // sigla
+        const a = document.createElement('a');
+        a.classList.add('curse');
+        a.id = curso.sigla.toLowerCase();
+
+        const img = document.createElement('img');
+        img.src = curso.icone;
+
+        const span = document.createElement('span');
+        span.classList.add('curse-text');
+        span.textContent = curso.sigla
+
+        a.append(img) // a -> img
+        a.append(span) // a --> span
+
+        a.addEventListener('click', (event) => {
+            const id = event.target.id;
+
+            localStorage.setItem('cursoID', id); // valor salvo no browser
+
+            location.href = 'html/secondpage.html'
+        })
+
+        containerCards.append(a)
+
+    })
+}
+
+createCards(cursos)
+
+
+// pegar o valor do localStorage
+// const valor = localStorage.getItem('cursoID')
