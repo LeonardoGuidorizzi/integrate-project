6746b1169bed3781560de7b03a38c1609d506499
LEONARDO MOREIRA GUIDORIZZI <22102368@dsn127macprof.cfp127aluno.local>
2022-10-04 11:28:22 -0300

    comiu

--- frontend/html/secondPageCards.js
+++ frontend/html/secondPageCards.js
@@ -9,7 +9,7 @@ const fetchAlunos = async (siglaCurso) => {
 }
 
 const { alunos } = await fetchAlunos(curso)
-console.log(alunos);
+// console.log(alunos);
 
 // console.log(alunos)
 // funcao -> pegar o container dos cards / criar uma div para conter as informacoes / dentro da div -> criar uma tag de img para a foto do estudante / criar uma tag de span para o nome do estudante / adicionar os dois elementos na div e adiciona a div no container
@@ -42,7 +42,13 @@ const createCards = (aluno) => {
     const container = document.querySelector('.card-container')
 
     const div = document.createElement('div')
-    div.classList.add('cards')
+    div.classList.add('card')
+
+    if (aluno.status.toLowerCase() === 'cursando') {
+        div.classList.add('cursando')
+    } else if (aluno.status.toLowerCase() === 'finalizado') {
+        div.classList.add('finalizado')
+    }
 
     const fotoAluno = document.createElement('img')
     fotoAluno.src = aluno.foto  
@@ -59,3 +65,11 @@ const createCards = (aluno) => {
 }
 
 alunos.forEach(createCards)
+
+const filtroStatus = document.querySelector('.select-status');
+let valorFiltroStatus = document.querySelector('.select-status').value;
+
+filtroStatus.addEventListener('change', () => {
+    valorFiltroStatus = document.querySelector('.select-status').value;
+    console.log(valorFiltroStatus);
+})
